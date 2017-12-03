68458165ab602ca8c38df87c8f9ae8f09ae7d830
TarasVolianskyi <volianskyitv@gmail.com>
2017-12-03 23:36:39 +0200

    complite with synchronization

--- app/src/main/java/com/volianskyi/taras/testfirebase/MainActivity.java
+++ app/src/main/java/com/volianskyi/taras/testfirebase/MainActivity.java
@@ -8,7 +8,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnAddData;
     private EditText etMail;
     private EditText etPass;
-private ProgressDialog progressDialog;
+    private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
 
     public MainActivity() {
@@ -38,7 +37,7 @@ private ProgressDialog progressDialog;
     }
 
     private void initView() {
-firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
 
         btnAddData = (Button) findViewById(R.id.btnAddDataMainActivity);
@@ -46,10 +45,7 @@ firebaseAuth = FirebaseAuth.getInstance();
         etPass = (EditText) findViewById(R.id.etPassMainActivity);
         btnAddData.setOnClickListener(this);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
 
     }
 
@@ -62,23 +58,28 @@ firebaseAuth = FirebaseAuth.getInstance();
         String mail = etMail.getText().toString().trim();
         String pass = etPass.getText().toString().trim();
 
-        if(TextUtils.isEmpty(mail)){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(mail);
+
+        myRef.setValue(pass);
+
+        if (TextUtils.isEmpty(mail)) {
             Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show();
-       return;
+            return;
         }
-        if(TextUtils.isEmpty(pass)){
+        if (TextUtils.isEmpty(pass)) {
             Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show();
-       return;
+            return;
         }
         progressDialog.setMessage("Registration of user...");
         progressDialog.show();
 
-        firebaseAuth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        firebaseAuth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Toast.makeText(MainActivity.this, "Registread successful", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     Toast.makeText(MainActivity.this, "Registread Unsuccessful", Toast.LENGTH_SHORT).show();
 
                 }
